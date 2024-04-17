fcb99851524d2320dd9ba6c026fb74f0726d12c0
vaiddoorya <147363522+vaiddoorya@users.noreply.github.com>
2024-04-17 22:20:05 +0530

    Add files via upload

---
+++ hangman/words.py
@@ -0,0 +1,12 @@
+word_list=["apple", "banana", "cat", "dog", "house", "car", "tree", "book", "pen", "table",
+    "chair", "ball", "sun", "moon", "star", "water", "flower", "bird", "fish", "cake",
+    "milk", "bread", "hat", "shoe", "sock", "door", "key", "phone", "clock", "lamp",
+    "chair", "desk", "bed", "shirt", "pants", "socks", "hat", "glove", "jacket", "coat",
+    "scarf", "glasses", "mirror", "knife", "fork", "spoon", "plate", "cup", "glass",
+    "potato", "tomato", "onion", "carrot", "banana", "apple", "orange", "pear", "grape",
+    "strawberry", "melon", "kiwi", "lemon", "peach", "plum", "cherry", "blueberry",
+    "raspberry", "watermelon", "pineapple", "coconut", "mango", "avocado", "peanut",
+    "almond", "cashew", "walnut", "honey", "sugar", "salt", "pepper", "flour", "rice",
+    "bread", "butter", "cheese", "egg", "milk", "yogurt", "chicken", "beef", "fish",
+    "pork", "lamb", "duck", "turkey", "goose", "rabbit", "frog", "snake", "turtle",
+    "elephant", "lion", "tiger", "giraffe", "zebra", "monkey", "bear", "kangaroo"]
